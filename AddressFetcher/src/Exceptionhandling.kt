7f3e7d391c93b0640a3fdb4204ea9654a32dcacb
7Snake-Eyes7 <53563576+7Snake-Eyes7@users.noreply.github.com>
2020-04-08 02:23:16 +0530

    Exception handled has been added

--- AddressFetcher/src/Exceptionhandling.kt
+++ AddressFetcher/src/Exceptionhandling.kt
@@ -1,2 +1,6 @@
 package com.vayana
 
+val urlRequired:String? = "Please setup db url in run time environment"
+val driverRequired:String? = "Please setup driver in run time environment"
+val userNameRequired:String? = "incorrect username or please setup user name in environment variable"
+val passwordRequired:String? = "incorrect password or setup password in environment variable"
